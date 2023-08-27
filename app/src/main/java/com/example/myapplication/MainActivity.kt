3264a4a2d0f94d1b072e55fa264d02e0e61051f2
wndudwkd003 <ymail3@naver.com>
2023-08-27 21:19:55 +0900

    수정

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,14 +15,13 @@ class MainActivity : AppCompatActivity() {
 
         val imageSlider = findViewById<ImageSlider>(R.id.imageSlider)
         val items = listOf(
-            R.drawable.test_image_1,
-            R.drawable.test_image_2,
-            R.drawable.test_image_3,
-            R.drawable.test_image_4
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background
         )
 
         imageSlider.imageItemList = items
-        imageSlider.imageScaleType = ImageView.ScaleType.CENTER
         imageSlider.setOnImageClickListener(object : ImageSlider.OnImageClickListener {
             override fun onImageClick(position: Int, context: Context) {
                 Toast.makeText(context, position.toString() + " Click", Toast.LENGTH_SHORT).show()
